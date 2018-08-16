eabb9c2fa21bd3916ee626fcd6a702987d9ff18e
Stefan Eng <eng_steff@hotmail.com>
2018-08-16 14:09:09 +0200

    Get correct tile circles.

--- hmm.py
+++ hmm.py
@@ -19,22 +19,41 @@ DICT_DIRECTIONS = {DIR : 0.0 for DIR in DIRECTIONS}
 
 matrix = [[DICT_DIRECTIONS]*NUM_COLS for _ in range(NUM_ROWS)]
 
+def is_robot_locotion(x, y):
+    return x == POS_ROBOT.x and y == POS_ROBOT.y
+
 def read_sensor():
 
     def get_circle_dist_from_robot(tiles):
         xs = range(POS_ROBOT.x - tiles, POS_ROBOT.x + tiles + 1)
         ys = range(POS_ROBOT.y - tiles, POS_ROBOT.y + tiles + 1)
+
+        def not_max_or_min(x, y):
+            x_not_max_or_min = x not in [min(xs), max(xs)]
+            y_not_may_or_min = y not in [min(ys), max(ys)]
+            return x_not_max_or_min and y_not_may_or_min
+
+        def less_than_zero(x, y):
+            return x < 0 or y < 0
+
+        def invalid_coords(x,y):
+            return any([
+                not_max_or_min(x,y),
+                less_than_zero(x,y),
+                is_robot_locotion(x,y),
+            ])
+
         tiles = []
         for y in ys:
             for x in xs:
-                print(x,y)
-                if (x < 0 or y < 0) or (POS_ROBOT.x == x and POS_ROBOT.y == y):
-                    continue
-                tiles.append((x,y))
+                if not invalid_coords(x,y):
+                    tiles.append((x,y))
         return tiles
 
     Ls1 = get_circle_dist_from_robot(1)
+    print(Ls1)
     Ls2 = get_circle_dist_from_robot(2)
+    print(Ls2)
 
 
 def main():
