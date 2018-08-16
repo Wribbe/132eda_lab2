af484879e58baf600aeb64d8f0c80aa8addaaa49
Stefan Eng <eng_steff@hotmail.com>
2018-08-16 13:28:31 +0200

    Set up basic data structure for matrix.

--- hmm.py
+++ hmm.py
@@ -1,7 +1,17 @@
 #!/bin/env python3
 
+NUM_ROWS = 8
+NUM_COLS = 8
+
+DIRECTIONS = ['N', 'W', 'S', 'E']
+N, W, S, E = DIRECTIONS
+
+DICT_DIRECTIONS = {DIR : 0.0 for DIR in DIRECTIONS}
+
 def main():
-    print("HELLO")
+
+    matrix = [[DICT_DIRECTIONS]*NUM_COLS for _ in range(NUM_ROWS)]
+    print(matrix)
 
 if __name__ == "__main__":
     main()
