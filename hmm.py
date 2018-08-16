bd532605b0969c95ee16ccc8d46cb2e06fb5929a
Stefan Eng <eng_steff@hotmail.com>
2018-08-16 15:39:58 +0200

    Move more functionality into the robot class.

--- hmm.py
+++ hmm.py
@@ -37,26 +37,9 @@ class Robot:
     def facing_wall(self):
         return out_of_bounds(self.next_pos())
 
-ROBOT = Robot(ROBOT_START_X, ROBOT_START_Y, ROBOT_START_HEADING)
-
-DICT_DIRECTIONS = {DIR : 0.0 for DIR in DIRECTIONS}
-
-matrix = [[DICT_DIRECTIONS]*NUM_COLS for _ in range(NUM_ROWS)]
-
-def out_of_bounds(x, y):
-    return (x < 0 or y < 0) or (x >= NUM_COLS or y >= NUM_ROWS)
-
-def get_robot_location():
-    return (ROBOT.x, ROBOT.y)
-
-def is_robot_locotion(x, y):
-    return (x, y) == get_robot_location()
-
-def read_sensor():
-
-    def get_circle_dist_from_robot(tiles):
-        xs = range(ROBOT.x - tiles, ROBOT.x + tiles + 1)
-        ys = range(ROBOT.y - tiles, ROBOT.y + tiles + 1)
+    def get_circle_dist_from_robot(self, tiles):
+        xs = range(self.x - tiles, self.x + tiles + 1)
+        ys = range(self.y - tiles, self.y + tiles + 1)
 
         def not_max_or_min(x, y):
             x_not_max_or_min = x not in [min(xs), max(xs)]
@@ -67,7 +50,7 @@ def read_sensor():
             return any([
                 not_max_or_min(x,y),
                 out_of_bounds(x,y),
-                is_robot_locotion(x,y),
+                self.is_robot_locotion(x,y),
             ])
 
         tiles = []
@@ -77,48 +60,59 @@ def read_sensor():
                     tiles.append((x,y))
         return tiles
 
-    Ls1 = get_circle_dist_from_robot(1)
-    Ls2 = get_circle_dist_from_robot(2)
+    def read_sensor(self):
 
-    ROBOT.Ls1 = Ls1
-    ROBOT.Ls2 = Ls2
+        Ls1 = self.get_circle_dist_from_robot(1)
+        Ls2 = self.get_circle_dist_from_robot(2)
 
-    prob_Ls1_upper = 0.1 + 0.05 * len(Ls1)
-    prob_Ls2_upper = prob_Ls1_upper + 0.025 * len(Ls2)
+        self.Ls1 = Ls1
+        self.Ls2 = Ls2
 
-    ROBOT.prob_Ls1_upper = prob_Ls1_upper
-    ROBOT.prob_Ls2_upper = prob_Ls2_upper
+        prob_Ls1_upper = 0.1 + 0.05 * len(Ls1)
+        prob_Ls2_upper = prob_Ls1_upper + 0.025 * len(Ls2)
 
-    die = random.uniform(0.0, 1.0)
-    if 0.0 <= die <= 0.1:
-        return get_robot_location()
-    elif 0.1 < die <= prob_Ls1_upper:
-        return random.choice(Ls1)
-    elif prob_Ls1_upper < die <= prob_Ls2_upper:
-        return random.choice(Ls2)
-    else:
-        return (None, None)
+        self.prob_Ls1_upper = prob_Ls1_upper
+        self.prob_Ls2_upper = prob_Ls2_upper
 
-def check_probabilites():
+        die = random.uniform(0.0, 1.0)
+        if 0.0 <= die <= 0.1:
+            return self.get_robot_location()
+        elif 0.1 < die <= prob_Ls1_upper:
+            return random.choice(Ls1)
+        elif prob_Ls1_upper < die <= prob_Ls2_upper:
+            return random.choice(Ls2)
+        else:
+            return (None, None)
+
+    def get_robot_location(self):
+        return (self.x, self.y)
+
+    def is_robot_locotion(self, x, y):
+        return (x, y) == self.get_robot_location()
+
+def out_of_bounds(x, y):
+    return (x < 0 or y < 0) or (x >= NUM_COLS or y >= NUM_ROWS)
+
+def check_probabilites(robot):
 
     count = Counter()
     total = 1e4
     for _ in range(int(total)):
-        count[read_sensor()] += 1
+        count[robot.read_sensor()] += 1
     print("Current probabilities: ")
     fmt = "{}: {}%"
     print(fmt.format("correct", 10.0))
     for (text, value) in zip(["prob_L1", "prob_L2", "prob_nothing"],
-                             [v*100 for v in ROBOT.probabilites()]):
+                             [v*100 for v in robot.probabilites()]):
         print(fmt.format(text, value))
 
     count_rings = Counter()
     for coord, value in count.items():
-        if coord in ROBOT.Ls1:
+        if coord in robot.Ls1:
             count_rings["Ls1"] += value
-        elif coord in ROBOT.Ls2:
+        elif coord in robot.Ls2:
             count_rings["Ls2"] += value
-        elif coord == get_robot_location():
+        elif coord == robot.get_robot_location():
             count_rings["True Location"] += value
         else:
             count_rings["Nothing"] += value
@@ -131,7 +125,12 @@ def check_probabilites():
 
 def main():
 
-    #check_probabilites()
+    ROBOT = Robot(ROBOT_START_X, ROBOT_START_Y, ROBOT_START_HEADING)
+    DICT_DIRECTIONS = {DIR : 0.0 for DIR in DIRECTIONS}
+    MATRIX = [[DICT_DIRECTIONS]*NUM_COLS for _ in range(NUM_ROWS)]
+
+    check_probabilites(ROBOT)
+
     return 0
 
 if __name__ == "__main__":
